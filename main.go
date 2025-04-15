1642edd751958d8a422b8e62c6576db0355033ed
Gerrit <tirreg91@googlemail.com>
2025-04-15 15:24:07 +0200

    Linter.

--- main.go
+++ main.go
@@ -94,7 +94,9 @@ func getMetrics(ctx context.Context) (*metrics, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error executing http request: %w", err)
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	raw, err := io.ReadAll(resp.Body)
 	if err != nil {
